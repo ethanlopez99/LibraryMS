7644381175ad7ba10f0a8003174e919c31031100
Izan <e.lopezruiz123@gmail.com>
2023-11-21 23:29:49 +0000

    Updated Book model, changed 'book_name' to 'title'. Removed lender_id from BookCreate model.

--- backend/app/api/models.py
+++ backend/app/api/models.py
@@ -22,7 +22,7 @@ class Book(Base):
     __tablename__ = "books"
 
     id = Column(Integer, primary_key=True, index=True)
-    book_name = Column(String(96), unique=True, index=True) # Max length calculated at 5 words title + 7 words subtitle, each word 8 letters
+    title = Column(String(96), unique=True, index=True) # Max length calculated at 5 words title + 7 words subtitle, each word 8 letters
     author = Column(String(50))
     is_available = Column(Boolean, default=True)
     lender_id = Column(Integer, ForeignKey('lenders.id'))
@@ -34,7 +34,6 @@ class Book(Base):
 class BookCreate(BaseModel):
     title: str
     author: str
-    lender_id: str
 
 
 # Lender object created as a table in database. Each lender object can have multiple books.
