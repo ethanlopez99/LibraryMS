8e067b1c4b667892f87cb2dfd937ad1cbb65fe11
Izan <e.lopezruiz123@gmail.com>
2023-11-22 00:17:31 +0000

    Fixed bug in transactions.py

--- backend/app/api/transactions.py
+++ backend/app/api/transactions.py
@@ -10,7 +10,10 @@ router = APIRouter(prefix="/transactions")
 def new_transaction(transaction: TransactionCreate, db: Session = Depends(get_db), token: dict = Depends(security.verify_token)):
     if "sub" not in token:
         raise HTTPException(status_code=401, detail="Not Authorized")
-    return crud.create_transaction(db, transaction)
+
+    transaction_data = {'book_id': transaction.book_id, 'lender_id': transaction.lender_id, 'transaction_type': transaction.transaction_type}
+
+    return crud.new_transaction(db, transaction_data)
 
 @router.get("/")
 def get_transactions(skip: int = 0, limit: int = 10, db: Session = Depends(get_db), token: dict = Depends(security.verify_token)):
