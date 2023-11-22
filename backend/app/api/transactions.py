7cd252f955ab6f35da11e1d5feaa91386b085af8
Izan <e.lopezruiz123@gmail.com>
2023-11-22 20:50:20 +0000

    Added error handling in new transaction route

--- backend/app/api/transactions.py
+++ backend/app/api/transactions.py
@@ -13,7 +13,11 @@ def new_transaction(transaction: TransactionCreate, db: Session = Depends(get_db
 
     transaction_data = {'book_id': transaction.book_id, 'lender_id': transaction.lender_id, 'transaction_type': transaction.transaction_type}
 
-    return crud.new_transaction(db, transaction_data)
+    new_transaction_data = crud.new_transaction(db, transaction_data)
+    if (new_transaction_data):
+        return new_transaction_data
+    
+    raise HTTPException(status_code=422, detail="Unprocessable Data")
 
 @router.get("/")
 def get_transactions(skip: int = 0, limit: int = 10, db: Session = Depends(get_db), token: dict = Depends(security.verify_token)):
