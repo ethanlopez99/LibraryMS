a4c371cd063ac515965de3025904325ce3b75d44
Izan <e.lopezruiz123@gmail.com>
2023-11-22 21:43:55 +0000

    Initial Front End setup

---
+++ frontend/src/screens/HomeScreen/HomeScreen.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const HomeScreen = ({ userToken, setUserToken }) => {
+  const handleLogout = () => {
+    // Remove user token, log out
+  };
+  return <div>HomeScreen</div>;
+};
+
+export default HomeScreen;
---
+++ frontend/src/screens/LoginScreen/LoginScreen.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const LoginScreen = ({ setUserToken }) => {
+  const handleLogin = async ({ username, password }) => {
+    // Login Function to go here
+  };
+
+  return <div>LoginScreen</div>;
+};
+
+export default LoginScreen;
