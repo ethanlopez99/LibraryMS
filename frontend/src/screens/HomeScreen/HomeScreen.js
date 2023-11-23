87c4f2b023e81e0b1f0bc3cefc5fc8a4e1073ba7
Izan <e.lopezruiz123@gmail.com>
2023-11-23 00:16:02 +0000

    Added Log Out Button and first MenuItem component

--- frontend/src/screens/HomeScreen/HomeScreen.js
+++ frontend/src/screens/HomeScreen/HomeScreen.js
@@ -1,10 +1,21 @@
 import React from "react";
+import axios from "axios";
+import MenuItem from "../../components/MenuItem/MenuItem";
+
+import { PiBooksFill } from "react-icons/pi";
 
 const HomeScreen = ({ userToken, setUserToken }) => {
   const handleLogout = () => {
-    // Remove user token, log out
+    setUserToken(null);
   };
-  return <div>HomeScreen</div>;
+  return (
+    <div>
+      <button type="button" onClick={handleLogout}>
+        Log Out
+      </button>
+      <MenuItem Icon={PiBooksFill} title="Books" value={10} color="green" />
+    </div>
+  );
 };
 
 export default HomeScreen;
