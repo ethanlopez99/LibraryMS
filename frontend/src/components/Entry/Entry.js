53fdd60548947a8d7f1b9976a9b6d284986c83ea
Izan Lopez <e.lopezruiz123@gmail.com>
2023-11-26 18:43:34 +0000

    Updated Entry to contain edit button

--- frontend/src/components/Entry/Entry.js
+++ frontend/src/components/Entry/Entry.js
@@ -1,20 +1,31 @@
 import React from "react";
 import "./Entry.css";
 
+import { BiSolidPencil } from "react-icons/bi";
+
 const Entry = ({ name, id }) => {
   return (
     <div className="entry">
       <p style={{ textAlign: "left", flex: 1 }}>{name}</p>
-      <p
+      <div
         style={{
-          color: "grey",
-          fontStyle: "italic",
-          textAlign: "right",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "flex-end",
           flex: 1,
         }}
       >
-        id: {id}
-      </p>
+        <p
+          style={{
+            color: "grey",
+            fontStyle: "italic",
+            textAlign: "right",
+          }}
+        >
+          id: {id}
+        </p>
+        <BiSolidPencil className="edit_button" />
+      </div>
     </div>
   );
 };
