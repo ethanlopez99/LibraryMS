31239c0b4d75b558f6040667a2a7235d2506922c
ethanlopez99 <e.lopezruiz123@gmail.com>
2023-11-28 21:26:35 +0100

    Added comments

--- frontend/src/components/Modals/TransactionsModal/TransactionsModal.js
+++ frontend/src/components/Modals/TransactionsModal/TransactionsModal.js
@@ -2,11 +2,12 @@
 import BooksModal from "../BooksModal/BooksModal";
 
 
-function TransactionsModal({userToken, setTransactionsModalShow, getNumberOfLoans}){
+function TransactionsModal({userToken, setTransactionsModalShow}){
 
 
     return (
         <>
+        {/* Calls BooksModal, with unavailable prop set to true, to find all unavailable books */}
             <BooksModal unavailable={true} setTransactionsModalShow={setTransactionsModalShow} userToken={userToken} />
         </>
   );
