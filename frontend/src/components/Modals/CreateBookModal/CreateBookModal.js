4775884475806f1d518f9a1b2fab7d478839ea64
ethanlopez99 <e.lopezruiz123@gmail.com>
2023-11-28 21:07:19 +0100

    Added comments to create book and lender modals

--- frontend/src/components/Modals/CreateBookModal/CreateBookModal.js
+++ frontend/src/components/Modals/CreateBookModal/CreateBookModal.js
@@ -13,33 +13,41 @@ const CreateBookModal = ({
   userToken,
   getNumberOfBooks,
 }) => {
+  // define message for future use
   const [message, setMessage] = useState(false);
 
+
+  // new validation schema for formik form
   const validationSchema = Yup.object().shape({
     book_name: Yup.string().required("Required").label("Book Name"),
     author: Yup.string().required("Required").label("Author"),
   });
 
+  // handles submit of new book
   const handleSubmit = async (values, { resetForm }) => {
+    // New book values are stored in dictionary suitable for API request
     const new_book = { title: values.book_name, author: values.author };
     try {
+      // post request is made to API endpoint
       const response = await axios.post(
         "http://127.0.0.1:8000/books/new",
         new_book,
         { headers: { Authorization: `Bearer ${userToken}` } }
       );
+      // if successful, update dashboard number of books and reset form
       getNumberOfBooks();
       resetForm();
+      // add message to let user know successful creation and new book id
       setMessage({
         message: `New Book ${response.data.title} created with id ${response.data.id}`,
         color: "green",
       });
     } catch (error) {
+      // if error, let user know there was an issue with creation of book
       setMessage({
         message: "Unable to create book. Please try again",
         color: "red",
       });
-      console.log(error);
     }
   };
 
@@ -68,6 +76,7 @@ const CreateBookModal = ({
                 placeholder="Title"
                 className="app_login-form_input_field"
               />
+              {/* error message shown if input does not follow validation */}
               <ErrorMessage name="book_name" component="div" />
             </div>
             <div className="app_login-form_field_container">
@@ -78,6 +87,7 @@ const CreateBookModal = ({
                 placeholder="Author"
                 className="app_login-form_input_field"
               />
+              {/* error message shown if input does not follow validation */}
               <ErrorMessage name="author" component="div" />
             </div>
             <button type="submit" className="app_login-form_submit">
--- frontend/src/components/Modals/CreateLenderModal/CreateLenderModal.js
+++ frontend/src/components/Modals/CreateLenderModal/CreateLenderModal.js
@@ -13,27 +13,40 @@ const CreateLenderModal = ({
   userToken,
   getNumberOfLenders,
 }) => {
+  // define message for future use
   const [message, setMessage] = useState(false);
 
+  // new validation schema for formik form
   const validationSchema = Yup.object().shape({
     lender_name: Yup.string().required("Required").label("Book Name"),
   });
 
-  const handleSubmit = async (values) => {
-    const new_book = { lender_name: values.lender_name };
+  // handles submit of new lender
+  const handleSubmit = async (values, {resetForm}) => {
+    // New lender values are stored in dictionary suitable for API request
+    const new_lender = { lender_name: values.lender_name };
     try {
       const response = await axios.post(
+      // post request is made to API endpoint
         "http://127.0.0.1:8000/lenders/new",
-        new_book,
+        new_lender,
         { headers: { Authorization: `Bearer ${userToken}` } }
       );
+      // if successful, update dashboard number of lenders and reset form
       getNumberOfLenders();
-      console.log(response);
+      resetForm();
+      // add message to let user know successful creation and new lender id
       setMessage({
         message: `New Lender ${response.data.lender_name} created with id ${response.data.id}`,
         color: "green",
       });
-    } catch (error) {}
+    } catch (error) {
+      // if error, let user know there was an issue with creation of book
+      setMessage({
+        message: "Unable to create lender. Please try again",
+        color: "red",
+      });
+    }
   };
 
   return (
@@ -61,6 +74,7 @@ const CreateLenderModal = ({
                 placeholder="Name"
                 className="app_login-form_input_field"
               />
+              {/* error message shown if input does not follow validation */}
               <ErrorMessage name="lender_name" component="div" />
             </div>
             <button type="submit" className="app_login-form_submit">
