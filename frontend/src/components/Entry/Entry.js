91e78580c8eb2ea1387b7e5cfa5c58a34cd6a9d8
Izan Lopez <e.lopezruiz123@gmail.com>
2023-11-30 19:53:13 +0000

    Added error handling from api

--- frontend/src/components/Entry/Entry.js
+++ frontend/src/components/Entry/Entry.js
@@ -3,138 +3,147 @@ import "./Entry.css";
 
 import { BiSolidPencil } from "react-icons/bi";
 
-const Entry = ({ book, handleUpdate, lender, setErrorMessage, editable = true, count }) => {
+const Entry = ({
+  book,
+  handleUpdate,
+  lender,
+  setErrorMessage,
+  editable = true,
+  count,
+}) => {
   const [editMode, setEditMode] = useState(false);
   const [editedTitle, setEditedTitle] = useState();
-  const [editedAuthor, setEditedAuthor] = useState();    
+  const [editedAuthor, setEditedAuthor] = useState();
   const [editedLenderName, setEditedLenderName] = useState();
 
-
-useEffect(() => {
-  if (book) {
-    setEditedTitle(book.title)
-    setEditedAuthor(book.author)
-  } else if (lender) {
-    setEditedLenderName(lender.lender_name)
-  }
-}, [])
+  useEffect(() => {
+    if (book) {
+      setEditedTitle(book.title);
+      setEditedAuthor(book.author);
+    } else if (lender) {
+      setEditedLenderName(lender.lender_name);
+    }
+  }, []);
 
   const handleEditClick = () => {
     setEditMode(true);
   };
 
   const handleSaveClick = () => {
-    if (book) {
-      if (!editedAuthor || !editedTitle) {
-        setErrorMessage({message: "Please do not leave fields blank", color: 'red'})
-        return null
-      }
-    } else if (lender) {
-      if (!editedLenderName) {
-        setErrorMessage({message: "Please do not leave fields blank", color: 'red'})
-        return null  
-      }
-    }
-
     // Call the handleUpdate function with the updated data
     if (book) {
-
       handleUpdate({ ...book, author: editedAuthor, title: editedTitle });
     } else if (lender) {
-      handleUpdate({...lender, lender_name: editedLenderName})
+      handleUpdate({ ...lender, lender_name: editedLenderName });
     }
 
     // Exit edit mode
     setEditMode(false);
     setErrorMessage();
+    // Reset if fail
+    if (book) {
+      setEditedTitle(book.title);
+      setEditedAuthor(book.author);
+    } else if (lender) {
+      setEditedLenderName(lender.lender_name);
+    }
   };
 
   if (book) {
-
     return (
       <div className="entry">
-      {editMode ? (
-        <>
-          <input
-            type="text"
-            value={editedTitle}
-            onChange={(e) => setEditedTitle(e.target.value)}
+        {editMode ? (
+          <>
+            <input
+              type="text"
+              value={editedTitle}
+              onChange={(e) => setEditedTitle(e.target.value)}
             />
-          <input
-            type="text"
-            value={editedAuthor}
-            onChange={(e) => setEditedAuthor(e.target.value)}
+            <input
+              type="text"
+              value={editedAuthor}
+              onChange={(e) => setEditedAuthor(e.target.value)}
             />
-          <button onClick={handleSaveClick}>Save</button>
-        </>
-      ) : (
-        <>
-          <p style={{ textAlign: "left", flex: 1.5 }}>{book.title}</p>
-          <p style={{ textAlign: "left", flex: 0.8 }}>{book.author}</p>
-          {count && <p style={{ textAlign: "left", flex: 0.3 }}>{count}</p>}
+            <button onClick={handleSaveClick}>Save</button>
+          </>
+        ) : (
+          <>
+            <p style={{ textAlign: "left", flex: 1.5 }}>{book.title}</p>
+            <p style={{ textAlign: "left", flex: 0.8 }}>{book.author}</p>
+            {count && <p style={{ textAlign: "left", flex: 0.3 }}>{count}</p>}
 
-          <div
-            style={{
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "flex-end",
-              flex: 1,
-            }}
-            >
-            <p
+            <div
               style={{
-                color: "grey",
-                fontStyle: "italic",
-                textAlign: "right",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "flex-end",
+                flex: 1,
               }}
-              >
-              id: {book.id}
-            </p>
-            {editable && <BiSolidPencil className="edit_button" onClick={handleEditClick} />}
-          </div>
-        </>
-      )}
-    </div>
-  );
-} else if (lender) {
-  return (
-    <div className="entry">
-    {editMode ? (
-      <>
-        <input
-          type="text"
-          value={editedLenderName}
-          onChange={(e) => setEditedLenderName(e.target.value)}
-          />
-        <button onClick={handleSaveClick}>Save</button>
-      </>
-    ) : (
-      <>
-        <p style={{ textAlign: "left", flex: 1 }}>{lender.lender_name}</p>
-        <div
-          style={{
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "flex-end",
-            flex: 1,
-          }}
-          >
-          <p
-            style={{
-              color: "grey",
-              fontStyle: "italic",
-              textAlign: "right",
-            }}
             >
-            id: {lender.id}
-          </p>
-          {editable && <BiSolidPencil className="edit_button" onClick={handleEditClick} />}
-        </div>
-      </>
-    )}
-  </div>
-);
-}
+              <p
+                style={{
+                  color: "grey",
+                  fontStyle: "italic",
+                  textAlign: "right",
+                }}
+              >
+                id: {book.id}
+              </p>
+              {editable && (
+                <BiSolidPencil
+                  className="edit_button"
+                  onClick={handleEditClick}
+                />
+              )}
+            </div>
+          </>
+        )}
+      </div>
+    );
+  } else if (lender) {
+    return (
+      <div className="entry">
+        {editMode ? (
+          <>
+            <input
+              type="text"
+              value={editedLenderName}
+              onChange={(e) => setEditedLenderName(e.target.value)}
+            />
+            <button onClick={handleSaveClick}>Save</button>
+          </>
+        ) : (
+          <>
+            <p style={{ textAlign: "left", flex: 1 }}>{lender.lender_name}</p>
+            <div
+              style={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "flex-end",
+                flex: 1,
+              }}
+            >
+              <p
+                style={{
+                  color: "grey",
+                  fontStyle: "italic",
+                  textAlign: "right",
+                }}
+              >
+                id: {lender.id}
+              </p>
+              {editable && (
+                <BiSolidPencil
+                  className="edit_button"
+                  onClick={handleEditClick}
+                />
+              )}
+            </div>
+          </>
+        )}
+      </div>
+    );
+  }
 };
 
 export default Entry;
